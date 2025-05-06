3f5430a7a509449f4336867d7d11a6892950187b
JayminPandya <jayminpandya7777@gmail.com>
2025-05-06 11:18:04 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
         <p className="uppercase font-normal text-[#ffffff]/50">Welcome to</p>
         <h1 className="text-[#ffffff] text-5xl uppercase font-bold tracking-tight">Ashutosh chess academy</h1>
       </div>
-      <button className="px-6 py-2 bg-[#ffffff] rounded-lg text-xl text-white font-normal border border-[#000000]/30 border-b-4 hover:border-b-2 text-black">Get Started</button>
+      <button className="px-6 py-2 bg-[#ffffff] rounded-lg text-xl font-normal border border-[#000000]/30 border-b-4 hover:border-b-2 text-black">Get Started</button>
     </div>
   );
 }
