6c4abd83852b75ce22db4739fd30231725a30b5a
JayminPandya <jayminpandya7777@gmail.com>
2025-07-15 01:19:34 -0700

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,50 @@
+export interface IMenuItem {
+    text: string;
+    url: string;
+}
+
+export interface IBenefit {
+    title: string;
+    description: string;
+    imageSrc: string;
+    bullets: IBenefitBullet[]
+}
+
+export interface IBenefitBullet {
+    title: string;
+    description: string;
+    icon: JSX.Element;
+}
+
+export interface IPricing {
+    name: string;
+    price: number | string;
+    features: string[];
+}
+
+export interface IFAQ {
+    question: string;
+    answer: string;
+}
+
+export interface ITestimonial {
+    name: string;
+    role: string;
+    message: string;
+    avatar: string;
+}
+
+export interface IStats {
+    title: string;
+    icon: JSX.Element;
+    description: string;
+}
+
+export interface ISocials {
+    facebook?: string;
+    instagram?: string;
+    twitter?: string;
+    youtube?: string;
+    whatsapp?: string;
+    [key: string]: string | undefined;
+}
