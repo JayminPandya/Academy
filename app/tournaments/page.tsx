3261d3de796cec648a473d025149e990b684d488
JayminPandya <jayminpandya7777@gmail.com>
2025-07-17 01:12:52 +0530

    Add files via upload

---
+++ app/tournaments/page.tsx
@@ -0,0 +1,11 @@
+import TournamentPage from "@/components/Competition/Tournament";
+
+const HomePage: React.FC = () => {
+    return (
+        <>
+            <TournamentPage />
+        </>
+    );
+};
+
+export default HomePage;
