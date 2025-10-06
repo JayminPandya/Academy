593f241c83f0184e96d8d12cb0d861af4676ad01
Jaymin Pandya <jayminpandya7777@gmail.com>
2025-10-06 22:31:52 +0530

    Update siteDetails.ts

--- data/siteDetails.ts
+++ data/siteDetails.ts
@@ -2,10 +2,11 @@ export const siteDetails = {
     siteName: '',
     siteUrl: 'https://www.ashutoshchessacademy.in/',
     metadata: {
-        title: 'Asutosh Chess Academy',
+        title: 'Ashutosh Chess Academy',
         description: 'Asutosh Chess Academy empowers individuals with the skills and strategies to excel in chess.',
     },
     language: 'en-us',
     locale: 'en-US',
-    siteLogo: "/rook.svg", // or use a string for the logo e.g. "TechStartup"
+    siteLogo: "/ACA_Monogram.png", // or use a string for the logo e.g. "TechStartup"
+
 }
